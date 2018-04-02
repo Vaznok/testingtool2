14432da5a4c7a6d3f4d6751a4915bf43a8d77205
Vitaliy Vlasenko <vlasenko.vitaliy.l@gmail.com>
2018-04-02 12:25:26 +0300

    update UserDto

--- testing-tool-web/src/main/java/com/epam/rd/controllers/UserController.java
+++ testing-tool-web/src/main/java/com/epam/rd/controllers/UserController.java
@@ -2,17 +2,10 @@ package com.epam.rd.controllers;
 
 import com.epam.rd.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    @Autowired
-    private UserService userService;
-
-    @GetMapping("/users")
-    public String findAllUsers() {
-      return userService.findAll().toString();
-    }
 }
