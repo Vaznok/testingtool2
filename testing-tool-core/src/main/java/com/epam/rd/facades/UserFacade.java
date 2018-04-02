8051c14e12caaf54751b11644e54a50dafdb9213
Vitaliy Vlasenko <vlasenko.vitaliy.l@gmail.com>
2018-04-02 18:39:37 +0300

    project structure rebuilding

--- testing-tool-core/src/main/java/com/epam/rd/facades/UserFacade.java
+++ testing-tool-core/src/main/java/com/epam/rd/facades/UserFacade.java
@@ -5,6 +5,7 @@ import com.epam.rd.dto.UserDto;
 import java.util.List;
 
 public interface UserFacade {
+
     List<UserDto> findAll();
 
     UserDto findUserByEmail(String email);
--- testing-tool-web/src/main/java/com/epam/rd/controllers/UserController.java
+++ testing-tool-web/src/main/java/com/epam/rd/controllers/UserController.java
@@ -1,9 +1,7 @@
 package com.epam.rd.controllers;
 
-import com.epam.rd.data.access.domain.User;
 import com.epam.rd.dto.UserDto;
 import com.epam.rd.facades.DefaultUserFacade;
-import com.epam.rd.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +13,11 @@ import javax.validation.constraints.Email;
 public class UserController {
 
     @Autowired
-    private UserService userService;
-
-    @Autowired
     private DefaultUserFacade userFacade;
 
     @GetMapping("/users")
     public String findAllUsers() {
-        return userService.findAll().toString();
+        return userFacade.findAll().toString();
     }
 
     @GetMapping("/user/{email}")
