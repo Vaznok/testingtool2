8e576717560f1705bbf4757bcf020238ee4eca7b
Svitlana_Ovcharenko <Svitlana_Ovcharenko@epam.com>
2018-04-03 17:43:31 +0300

    small channges

--- testing-tool-web/src/main/java/com/epam/rd/Application.java
+++ testing-tool-web/src/main/java/com/epam/rd/Application.java
@@ -1,16 +1,20 @@
 package com.epam.rd;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @EnableJpaRepositories("com.*")
 @SpringBootApplication(scanBasePackages = "com.*")
 @EntityScan("com.*")
+@EnableJSONDoc
+@ComponentScan
 public class Application {
 
     @Bean
